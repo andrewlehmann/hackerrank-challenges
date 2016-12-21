7e0db7e2e933fa989269969a0f2ddff3e5f68c3d
Andrew Lehmann <andrewlehmann@users.noreply.github.com>
2016-12-20 20:46:42 -0600

    Update intro_to_map.py

--- Python Data Structures/Intro to map function/intro_to_map.py
+++ Python Data Structures/Intro to map function/intro_to_map.py
@@ -1,15 +1,21 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 import sys
+
+
 input = []
+
 for item in sys.stdin:
     input.append(item)
 A = set(map(int, input[2].split()))
 B = set(map(int, input[3].split()))
 input = map(int, input[1].split())
+
 total = 0
+
 for item in input:
     if item in A:
         total = total + 1
     if item in B:
         total = total - 1
+        
 print total
